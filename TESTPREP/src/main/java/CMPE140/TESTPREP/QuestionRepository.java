77f82f289b57b681abd92cd716c7a86c1d0c6895
Jtuck777 <90226322+Jtuck777@users.noreply.github.com>
2022-05-02 11:32:51 -0700

    Delete TESTPREP directory

--- TESTPREP/src/main/java/CMPE140/TESTPREP/QuestionRepository.java
+++
@@ -1,8 +0,0 @@
-package CMPE140.TESTPREP;
-
-public interface QuestionRepository {
-    public void insert (T entity);
-    public void  (T entity);
-    public void insert (T entity);
-    public void T FindbyNum(T entity);
-}
--- TESTPREP/src/main/java/CMPE140/TESTPREP/TEST.java
+++
@@ -1,9 +0,0 @@
-package CMPE140.TESTPREP;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class TEST {
-    List<Question> list = new ArrayList<Question>();
-
-}
